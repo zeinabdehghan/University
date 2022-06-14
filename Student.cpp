a3ed5516fb9c2e6204bd592a5811c498ae8f2699
zeinab <zeinabdehghani28@gmail.com>
2022-06-14 23:05:59 +0430

    validate & salary of student class

--- Student.cpp
+++ Student.cpp
@@ -2,22 +2,28 @@
 #include "Student.h"
 using namespace std;
 
-Student::Student(string fN ,string LN ,string id , double wh, Course c, string fos, int noc)  : Person(fN, LN ,id, wh) {
+Student::Student(string fN ,string LN ,string id , double wh, Course* c, string fos, int noc)  : Person(fN, LN ,id, wh) {
 
     FieldOfStudy = fos;
     numOfCourses = noc;
-    for(int i=0; i<numOfCourses; i++)
-    courses = new Course; *courses = c;
-}
+    for(int i=0; i<numOfCourses; i++){
+        courses = new Course;
+    courses[i] = c[i];}
+
+    if (!validate( id)) {
+        cout << "Invalid id";
+        exit(0);
+}}
 
 Student::Student(const Student &r) : Person(r) {
     FieldOfStudy= r.FieldOfStudy;
     numOfCourses = r.numOfCourses;
-    for(int i=0; i<numOfCourses; i++)
-        courses = new Course; *courses = *r.courses;
+    for(int i=0; i<numOfCourses; i++){
+        courses = new Course; courses[i] = r.courses[i];}
 }
+
 Student :: ~Student() {
-    delete this->courses;
+    delete  this->courses;
 }
 std::ostream &operator<<(ostream &output, Student s) {
     cout<< s.FieldOfStudy;
@@ -32,3 +38,39 @@ std::istream &operator>>(istream &input, Student &s) {
     cin >>*s.courses;
     return input;
 }
+
+bool Student::validate(string id) {
+    if (id [2]!= '*')
+        return 0;
+}
+    double Student::gpa() {
+        double sum =0;
+        int units=0;
+        for (int i = 0; i <numOfCourses; i++) {
+            sum += courses[i].get_mark() * courses[i].get_unit();
+        }
+       for (int i = 0; i <numOfCourses; i++){
+      units =  numOfCourses*courses[i].get_unit();
+        }
+       double gpa = sum/units;
+
+        return gpa;
+    }
+
+double Student::calculateSalary() {
+        double salary = (get_workHours()) *20000;
+        if (gpa()> 17)
+            salary+=salary/10;
+    return salary;
+}
+
+Student Student::operator=( Student & s1) {
+    Person ::operator=(s1);
+    FieldOfStudy = s1.FieldOfStudy;
+    numOfCourses = s1.numOfCourses;
+    for(int i=0; i<numOfCourses; i++){
+        courses = new Course; courses[i] = s1.courses[i];}
+}
+
+Student::Student() {}
+
--- Student.h
+++ Student.h
@@ -1,4 +1,3 @@
-
 #ifndef CPP_STUDENT_H
 #define CPP_STUDENT_H
 #include "Person.h"
@@ -9,13 +8,17 @@ class Student : public Person{
 private:
     Course* courses ; std::string FieldOfStudy; int numOfCourses;
 public:
-    Student(std :: string fN, std :: string LN , std ::string  id , double wh , Course c,std ::string fos , int noc);
+    Student(std :: string fN, std :: string LN , std ::string  id , double wh , Course * c,std ::string fos , int noc);
     Student(const Student & r);
-    ~Student();
-
+    Student();
+    ~Student() override;
     friend std:: ostream & operator << (std :: ostream &output,  Student s);
-
     friend std:: istream & operator >> (std:: istream &input,  Student & s);
+    bool validate(std :: string id) override ;
+    double gpa();
+    double calculateSalary() override;
+    Student operator = (Student &s1);
+
 };
 
 
